fa1592e9336b6112b50cd3f519915872c7e762b6
solucionesmedicasintegral-rgb <solucionesmedicasintegral@gmail.com>
2026-07-27 16:18:57 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'dr-morales-v1';
+const URLS_TO_CACHE = [
+  '/app-pacientes/',
+  '/app-pacientes/index.html'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(URLS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request);
+    })
+  );
+});
